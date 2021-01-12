dbedecb6e9431173457ded183a8fbdbd3e45931b
bpatel6 <bpatel6@uiowa.edu>
2021-01-12 12:50:50 -0600

    auto scheduled pulling data from web

--- coronavirus-tracker/src/main/java/io/java/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/io/java/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,20 +1,35 @@
 package io.java.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusDataService {
 
-    private static String data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+    private static String data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    @PostConstruct
+    @Scheduled(cron = "* * * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(data_url)).build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
-    
+
 }
